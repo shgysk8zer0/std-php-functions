a4fc5d12315b1e53f2fe0806536d8abdcf726d03
Chris Zuber <shgysk8zer0@gmail.com>
2015-05-05 19:03:50 -0700

    New error handler script
    Adds handlers to report error to:
    * Database
    * Log file
    * Developer console (browser... Admin only)

---
+++ error_handler.php
@@ -0,0 +1,34 @@
+<?php
+$errors   = new \shgysk8zer0\Core\Error_Event;
+$reporter = \shgysk8zer0\Core\Errors::load();
+
+$errors($reporter);
+
+array_map (
+	function($lvl) use ($reporter, $errors)
+	{
+		$errors->$lvl = [$reporter, $reporter::LOG_METHOD];
+	},
+	[
+		'error',
+		'warning',
+		'parse',
+		'notice',
+		'core_error',
+		'core_warning',
+		'compile_error',
+		'compile_warning',
+		'user_error',
+		'user_warning',
+		'user_notice'
+	]
+);
+if (is_ajax() and $login->logged_in and $login->role === 'admin') {
+	array_map(
+		function($e_level) use ($reporter, $errors)
+		{
+			$errors->$e_level = [$reporter, $reporter::CONSOLE_METHOD];
+		},
+		$errors::errorConstants()
+	);
+}
